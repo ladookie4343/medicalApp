ecdd7e31d35969f9a51f7b832d57cde6da34a604
ladookie4343 <mladuca@gmail.com>
2012-02-11 17:21:41 -0500

    Initial Commit

---
+++ medical/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  medical
+//
+//  Created by Matt LaDuca on 2/11/12.
+//  Copyright (c) 2012  PPA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ medical/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  medical
+//
+//  Created by Matt LaDuca on 2/11/12.
+//  Copyright (c) 2012  PPA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
