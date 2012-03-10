9ece2fb0927b8c171057f1b7d468e3f31a92fccb
ladookie4343 <mladuca@gmail.com>
2012-03-10 16:33:06 -0500

    foo

---
+++ medical/OfficeTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  OfficeTableViewController.h
+//  medical
+//
+//  Created by Matt LaDuca on 3/10/12.
+//  Copyright (c) 2012  PPA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OfficeTableViewController : UITableViewController
+
+@end
