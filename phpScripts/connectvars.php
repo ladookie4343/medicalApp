96f06fb588b931c97ad6d85f873d44acc7c1b274
ladookie4343 <mladuca@gmail.com>
2013-01-27 20:52:28 -0500

    Update phpScripts/connectvars.php

--- phpScripts/connectvars.php
+++ phpScripts/connectvars.php
@@ -1,8 +1,8 @@
 <?php
 
-   define('DB_HOST', 'ladookie4343usf.db.8864782.hostedresource.com');
-   define('DB_USER', 'ladookie4343usf');
-   define('DB_PASSWORD', 'M18TLkB5pX4cYF');
-   define('DB_NAME', 'ladookie4343usf');
+   define('DB_HOST', '');
+   define('DB_USER', '');
+   define('DB_PASSWORD', '');
+   define('DB_NAME', '');
 
 ?>
